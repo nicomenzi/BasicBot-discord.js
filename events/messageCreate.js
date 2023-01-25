dcb9adf2b8b024aa986bba47eecb6f79424a8235
Fynn Westerath <westerathf@gmail.com>
2023-01-25 17:03:51 +0100

    initial commit

--- events/messageCreate.js
+++
@@ -1,5 +0,0 @@
-const client = require("../index")
-
-client.on("messageCreate", async message => {
-    // console.log(message.author.displayName + "Has send a message with the value\n" + message.content);
-})
--- helpers/sleep.js
+++
@@ -1,5 +0,0 @@
-const { setTimeout } = require("timers")
-
-exports.sleep = async => (seconds) => {
-    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
-}
